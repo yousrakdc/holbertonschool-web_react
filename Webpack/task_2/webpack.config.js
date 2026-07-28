b27d929c694bc5cf9e8797ab1917abd7de0969a6
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-28 15:06:48 +0200

    -

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,36 +1,29 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./js/dashboard_main.js",
+  mode: 'production',
+  entry: {
+    main: './js/dashboard_main.js',
+  },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  performance: {
+    hints: false,
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              esModule: false,
-            },
-          },
-        ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              esModule: false,
-            },
-          },
-        ],
+        test: /\.(gif|jpe?g|png|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
       },
     ],
   },
