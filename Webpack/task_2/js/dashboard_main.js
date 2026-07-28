183940cc34d38b2b6abd196d8d12e3b255953c3e
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-28 15:05:43 +0200

    points

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,21 +1,19 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 import '../css/main.css';
 
 let count = 0;
-
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
+    count++;
+    $('#count').text(`${count} clicks on the button`);
 }
 
-$(document).ready(() => {
-  $('body').prepend("<div id='logo'></div>");
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('button').on('click', debounce(updateCounter, 500));
-});
+    $(function() {
+        $('body').append('<p>Holberton Dashboard</p>');
+        $('body').append('<p>Dashboard data for the students</p>');
+        $('body').append(`<button>Click here to get started</button>`);
+        $('body').append(`<p id="count"></p>`);
+        $('body').append(`<p>Copyright - Holberton School</p>`);
+        $('button').on('click', _.debounce(updateCounter, 500));
+    });
+    
