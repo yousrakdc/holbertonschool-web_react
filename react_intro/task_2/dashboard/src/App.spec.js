a20b41112f899d71455d12d3183ac658257592c7
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-30 14:16:58 +0200

    app spec updated

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -10,10 +10,27 @@ test('renders the h1 with text School Dashboard', async () => {
 test('renders login and copyright paragraph with the correct content', async () => {
     render(<App />)
     expect(screen.getByText(/^login to access the full dashboard$/i)).toBeInTheDocument()
-    expect(screen.getByText(new RegExp(`^copyright ${new Date().getFullYear()} - holberton school$`, 'i')))
+    expect(screen.getByText(/^copyright/i)).toBeInTheDocument()   
 })
 
 test('renders an img element', async () => {
     render(<App />)
     expect(screen.getByAltText(/^holberton logo$/i)).toBeInTheDocument()
+})
+
+test('renders email input and password input elements', async () => {
+    render(<App />)
+    expect(screen.getByRole('textbox', {name: /^email:$/i})).toBeInTheDocument()
+    expect(screen.getByLabelText(/^password:$/i)).toBeInTheDocument()
+})
+
+test('renders Email and Password label element', async () => {
+    render(<App />)
+    expect(screen.getByText(/^email:$/i)).toBeInTheDocument()
+    expect(screen.getByText(/^password:$/i)).toBeInTheDocument()
+})
+
+test('renders a button with text OK', async () => {
+    render(<App />)
+    expect(screen.getByRole('button', {name: /^ok$/i})).toBeInTheDocument()
 })
