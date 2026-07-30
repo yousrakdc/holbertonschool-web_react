7e5d10a03282cac16914e62fd39c6559fa4cbc41
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-30 14:32:58 +0200

    branch

---
+++ react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,21 @@
+const getCurrentYear = () => {
+    return new Date().getFullYear()
+}
+
+const getFooterCopy = (isIndex) => {
+    if (isIndex) {
+        return "Holberton School"
+    }
+    else {
+        return "Holberton School main dashboard"
+    }
+}
+
+const getLatestNotification = () => {
+    return "<strong>Urgent requirement</strong> - complete by EOD"
+}
+
+export { getCurrentYear,
+         getFooterCopy,
+         getLatestNotification,
+        }
