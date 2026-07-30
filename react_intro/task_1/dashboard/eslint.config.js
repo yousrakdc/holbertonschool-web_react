3caaa768a9620e2f90132acf5efd696d1418e533
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-07-30 15:10:09 +0200

    deployed

--- react_intro/task_1/dashboard/eslint.config.js
+++ react_intro/task_1/dashboard/eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from '../../task_4/dashboard/node_modules/eslint-plugin-react-hooks'
+import reactRefresh from '../../task_4/dashboard/node_modules/eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
