6d3b8b4bba3119f2c8a005a978a9cc8c2bf2f1c6
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-08-26 10:23:50 +0200

    testing

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -4,11 +4,11 @@ import { render, screen } from '@testing-library/react';
 describe('NotificationItem', () => {
     test('li element has the color blue when type is default', () => {
         render(<NotificationItem type="default" value="New course available" />);
-        expect(screen.getByText('New course available')).toHaveStyle({ color: 'blue' });
+        expect(screen.getByText('New course available')).toHaveStyle({ color: 'rgb(0, 0, 255)' });
     });
 
     test('li element has the color red when type is urgent', () => {
         render(<NotificationItem type="urgent" value="New resume available" />);
-        expect(screen.getByText('New resume available')).toHaveStyle({ color: 'red' });
+        expect(screen.getByText('New resume available')).toHaveStyle({ color: 'rgb(255, 0, 0)' });
     });
 });
